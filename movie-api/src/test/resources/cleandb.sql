be6e694cbcc499f4042a68b915b82cab6c072979
Ren Thao <lthao4@madisoncollege.edu>
2018-04-10 13:09:19 -0500

    added rating

--- movie-api/src/test/resources/cleandb.sql
+++ movie-api/src/test/resources/cleandb.sql
@@ -16,5 +16,5 @@ INSERT INTO Movies (id, title, description, releaseYear, director_id, genre_id, 
 INSERT INTO Movies (id, title, description, releaseYear, director_id, genre_id, rating_id) VALUES (3,  "test movie title 3", "test movie description 3", 2016, 1, 3, 1);
 INSERT INTO Movies (id, title, description, releaseYear, director_id, genre_id, rating_id) VALUES (4,  "should be first", "test movie description 4", 2016, 1, 3, 1);
 INSERT INTO Movies (id, title, description, releaseYear, director_id, genre_id, rating_id) VALUES (5,  "test movie title 5", "test movie description 5", 2016, 2, 3, 1);
-INSERT INTO Movies (id, title, description, releaseYear, director_id, genre_id, rating_id) VALUES (6,  "also first", "test movie description 3", 2016, 1, 3, 1);
-INSERT INTO Movies (id, title, description, releaseYear, director_id, genre_id, rating_id) VALUES (7,  "also first", "test movie description 3", 2016, 1, 3, 1);
+INSERT INTO Movies (id, title, description, releaseYear, director_id, genre_id, rating_id) VALUES (6,  "also first", "test movie description 3", 2016, 1, 3, 2);
+INSERT INTO Movies (id, title, description, releaseYear, director_id, genre_id, rating_id) VALUES (7,  "also first", "test movie description 3", 2016, 1, 3, 3);
